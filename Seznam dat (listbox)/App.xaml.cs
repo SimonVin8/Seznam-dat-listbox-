b9d2efad85cce96073b0539f9e2ad531d038a84f
Šimon Vinkler <24IA27_VINKLER@spsjc.cz>
2026-01-27 08:03:55 +0100

    Přidejte soubory projektu.

---
+++ Seznam dat (listbox)/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Seznam_dat__listbox_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
