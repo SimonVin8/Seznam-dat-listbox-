212cb57793a329f2a0db34df15de63a7878cddd3
Šimon Vinkler <24IA27_VINKLER@spsjc.cz>
2026-01-27 09:25:56 +0100

    cars listbox

--- Seznam dat (listbox)/MainWindow.xaml.cs
+++ Seznam dat (listbox)/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Seznam_dat__listbox_.Models;
+using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,59 @@ namespace Seznam_dat__listbox_
     /// </summary>
     public partial class MainWindow : Window
     {
+        //observable collection se sama refreshne při přidání nového prvku
+        public ObservableCollection<car> Cars { get; set; }
         public MainWindow()
         {
+            Cars = new();
             InitializeComponent();
+            ListData.ItemsSource = Cars;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string brand = ValidateTextInput(BrandInput.Text) ? BrandInput.Text : throw new Exception("BrandInput value is not supported");
+                string model = ValidateTextInput(ModelInput.Text) ? ModelInput.Text : throw new Exception("ModelInput value is not supported"); ;
+                if (!ValidateInteger(PowerInput.Text, out int power)) throw new Exception("PowerInput value is not supported");
+
+
+                car car = new car() { Brand = brand, Model = model, Power = power };
+                Cars.Add(car);
+                Clear();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message,"Error",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+            
+        }
+
+        private bool ValidateTextInput(string value)
+        {
+
+            return !(string.IsNullOrEmpty(value) || value.Length == 0);
+        }
+
+        private bool ValidateInteger(string value, out int number)
+        {
+            return int.TryParse(value, out number) && value.Length > 0;
+        }
+
+        public void Clear()
+        {
+            BrandInput.Text = ModelInput.Text = PowerInput.Text = string.Empty;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            car? selected = ListData.SelectedItem as car;   // ? -> povoluje hodnotu null
+            if (selected != null)
+            {
+                Cars.Remove(selected);
+            }
         }
     }
 }
---
+++ Seznam dat (listbox)/Models/car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seznam_dat__listbox_.Models
+{
+    public class car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Power { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Brand} {Model} ({Power}HP)";
+        }
+    }
+}
